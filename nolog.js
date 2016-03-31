1c36e9ccc5d775982558818ada5934848f397529
Andrew Gryaznov <ag@vrayo.com>
2016-03-31 21:22:09 +0300

    added protection for production

--- nolog.js
+++ nolog.js
@@ -1,5 +1,5 @@
 var manifest = chrome.runtime.getManifest();
-if(!("debug" in manifest && manifest.debug)) {
+if(!("debug" in manifest && manifest.debug) || 'update_url' in manifest) {
     console.log = function(){};
 }
 
