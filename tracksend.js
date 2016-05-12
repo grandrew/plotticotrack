48279f10d27e6d4b3a90c49f750062f9a825757f
Andrew Gryaznov <realgrandrew@gmail.com>
2016-05-12 09:19:20 +0300

    added license headers where absent

--- tracksend.js
+++ tracksend.js
@@ -1,3 +1,20 @@
+/*
+    This file is part of PlotticoTrack.
+
+    PlotticoTrack is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PlotticoTrack is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with PlotticoTrack. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /*global chrome,load_list,Image,UTILS,XPathEvaluator,XPathResult*/
 /*global recorder*/ // defined at recorder.js and included above
 /*global get_super_selector, execute_selector, b64_encode_safe, b64_decode_safe, xor*/
